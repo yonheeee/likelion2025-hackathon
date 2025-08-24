0eefb1455c73e75f5cbae17742c2fb28dc6f1684
minji <sksmalswl@gmail.com>
2025-08-25 02:09:19 +0900

    상세보기 정리 중

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ import Header from "./jsx/Header.jsx";
 import UserRecipt from "./jsx/user/receipt/UserRecipt.jsx";
 import UserCheck from "./jsx/user/check/UserCheck.jsx"
 import MyComplaint from './jsx/user/mycomplaint/MyComplaintsPage.jsx';
-import UserEntireComplaintLists from "./jsx/user/UserEntireComplaintLists/UserEntireComplaintLists.";
+import UserEntireComplaintLists from "./jsx/user/UserEntireComplaintLists/UserEntireComplaintLists.jsx";
+import UserDetailsPage from './jsx/user/UserDetail/UserDetail.jsx';
 
 import Adminmain from './jsx/admin/adminmainjsx/adminmain.jsx';
 import AdmindetailsPage from './jsx/admin/admindetailjsx/admindetails.jsx';
@@ -31,12 +32,12 @@ function App() {
             <Route path="/usercheck" element={<UserCheck />} /> {/* 민원 조회 */}
             <Route path="/mycomplaint" element={<MyComplaint />} /> {/* 나의 민원 */}
             <Route path="/user/entire" element={<UserEntireComplaintLists />} /> {/* 사용자 전체 민원 조회 */}
-            <Route path="/user/detail" element={<UserdetailsPage />} /> {/* 사용자 상세보기 */}
+            <Route path="/user/detail/:id" element={<UserDetailsPage />} /> {/* 사용자 상세보기 */}
 
 
             <Route path="/admin" element={<AdminFirstPage/>} /> {/* 관리자 처음 */}
             <Route path="/admin/main" element={<Adminmain />} /> {/* 관리자 메인 */}
-            <Route path="/admin/detail" element={<AdmindetailsPage />} /> {/* 관리자 상세보기 */}
+            <Route path="/admin/detail/:id" element={<AdmindetailsPage />} /> {/* 관리자 상세보기 */}
             <Route path="/admin/entire" element={<AdminEntireComplaintLists/>}/> {/* 관리자 전체 민원 조회 */}
           </Routes>
         </div>
